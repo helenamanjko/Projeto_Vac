9c579ba5c66eeb90dd49184924e84e37c2ad9ebb
Helena Manjko <helenamanjko@gmail.com>
2021-05-13 11:52:28 -0300

    Update Verificacao.java
    atualizando código -- ver idade -- ver dados corretos -- collections

--- Verificacao.java
+++ Verificacao.java
@@ -1,5 +1,7 @@
 package Project_Vac;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -20,13 +22,13 @@ public static void main(String[] args) {
 		int li, c;
 		int prioridade = 1; 
 		
-		Random gera = new Random(); // Classe Java que dentro dele tem uma funÁ„o de gerar numeros aleatÛrios
+		Random gera = new Random(); // Classe Java que dentro dele tem uma fun√ß√£o de gerar numeros aleat√≥rios
 		
 		System.out.println("========================================================");
 		System.out.println("                      Sys - Vac");
 		System.out.println("========================================================");
 		System.out.println("   Sistema de cadastro para idades entre 0 e 59 anos"
-				+" \n      Se vocÍ possui idade superior, ligue 1599"
+				+" \n      Se voc√™ possui idade superior, ligue 1599"
 				+" \n========================================================");
 		
 		//anda as linhas
@@ -34,41 +36,66 @@ public static void main(String[] args) {
 			
 			int aux =0;
 		
-			// InserÁ„o de dados
+			// Inser√ß√£o de dados
 			System.out.print("Nome: ");		
 			pe.setNome(l.next()); //  nome
 			//System.out.println(pe.getNome());
 			
-			System.out.print("EndereÁo: "); // endereÁo
-			pe.setEndereÁo(l.next());
+			System.out.print("Endere√ßo: "); // endere√ßo
+			pe.setEndere√ßo(l.next());
 			
 			System.out.print("Idade: ");
 			id.setIdade(l.nextInt());  //  idade
 			aux =id.getIdade();
 			
-			System.out.println(aux);
+			//System.out.println(aux);
 			
 			if(aux<60){
 			
-			System.out.print("Prioridade: " + id.condicao(id.getIdade()));
+			System.out.print("Prioridade: " + id.condicao(id.getIdade())); 
 			
-			System.out.println("\nQual È sua profiss„o?");	
-			System.out.println("[1]¡rea da Sa˙de\n[2]Motoristas/Cobrador\n[3]Profissional da EducaÁ„o\n[4]Profissional da SeguranÁa P˙blica"
+			//---------------------------idade menor de 18 --------------
+			
+				if(id.getIdade()<18) {
+					
+					System.out.print("\n=========================== Necess√°rio acompanhamento adulto ============================ "
+							+        "\n============== Se estiver acompanhado, atualize a p√°gina e insira seus dados =========== ");
+												
+				} 
+				else {
+					
+								
+			System.out.println("\nQual √© sua profiss√£o?");	
+			System.out.println("[1]√Årea da Sa√∫de\n[2]Motoristas/Cobrador\n[3]Profissional da Educa√ß√£o\n[4]Profissional da Seguran√ßa P√∫blica"
 					+ "\n[0]Outros ou Nenhum");
 			prof.setProfissoes(l.nextInt());
 			
 			System.out.print("Prioridade: " + prof.condicao(prof.getProfissoes()));
 			
-			System.out.println("\nPossui alguma doenÁa?");	
-			System.out.println("[1]Sindrome de Down\n[2]Problemas renais em di·lise\n[3]Transplantados Imunossuprimidos\n[4]Gr·vidas e PuÈperas com comorbidades\n[5]BPC\n[0]Nenhuma das OpÁıes");
+			System.out.println("\nPossui alguma doen√ßa?");	
+			System.out.println("[1]Sindrome de Down\n[2]Problemas renais em di√°lise\n[3]Transplantados Imunossuprimidos\n[4]Gr√°vidas e Pu√©peras com comorbidades\n[5]BPC\n[0]Nenhuma das Op√ß√µes");
 			esp.setEspeciais(l.nextInt());
 			
 			System.out.println(esp.condicao(esp.getEspeciais())); 
 			
+			//---------------------Collection - Integer-----------------
+			
+			Collection<Integer> condicao = new ArrayList<Integer>();
+			condicao.add(id.getIdade());
+			condicao.add(prof.getProfissoes());
+			condicao.add(esp.getEspeciais());
+
+			System.out.println("Suas op√ß√µes foram "+ ((ArrayList<Integer>) condicao).get(0) + " para idade, "
+					+ ((ArrayList<Integer>) condicao).get(1) + " para profiss√£o e " + ((ArrayList<Integer>) condicao).get(2) +
+					" para doen√ßas.");
+			System.out.println("======= Digite 1 para confirmar e prosseguir e 2 para sair ========");
+			int conf = l.nextInt();
+			
+			if(conf == 1) {
 			
 
 			/********************   prioridade    ****************************/	
-			//ser· recuperado no cadastro de cada pessoa			
+			//ser√° recuperado no cadastro de cada pessoa			
 			prioridade = (id.condicao(id.getIdade())); 
 			
 			if ( prioridade < prof.condicao(prof.getProfissoes())){
@@ -87,20 +114,20 @@ public static void main(String[] args) {
 			//                  ou
 			if (prioridade == 5 || prioridade == 4) { //se a prioridade for 4 ou 5
 				//					MAX - MIN        MIN
-				mes = gera.nextInt((7 - 5) +    1) + 5; //gera um mÍs entre 05 e 08
+				mes = gera.nextInt((7 - 5) +    1) + 5; //gera um m√™s entre 05 e 08
 				
 			}else if (prioridade == 3 || prioridade == 2) { ////se a prioridade for 4 ou 5
-				mes = gera.nextInt((10 - 8) + 1) + 8; //gera um mÍs entre 09 e 10
+				mes = gera.nextInt((10 - 8) + 1) + 8; //gera um m√™s entre 09 e 10
 				
 			}else {					
-				mes = gera.nextInt((12 - 11) + 1) + 11; //gera um mÍs entre 11 e 12
+				mes = gera.nextInt((12 - 11) + 1) + 11; //gera um m√™s entre 11 e 12
 			}				
 			
-			if (mes == 5){// se cair no mÍs atual	gerar o dia que n„o passou				
-				dia = gera.nextInt(30) + 15;//gerando um dia aleatÛrio
+			if (mes == 5){// se cair no m√™s atual	gerar o dia que n√£o passou				
+				dia = gera.nextInt(30) + 15;//gerando um dia aleat√≥rio
 			}
 			else {
-				dia = gera.nextInt(30) + 1;//gerando um dia aleatÛrio*/
+				dia = gera.nextInt(30) + 1;//gerando um dia aleat√≥rio*/
 				
 			}	
 			
@@ -126,13 +153,25 @@ public static void main(String[] args) {
 			for(c = 0; c < agenda[li].length; c++) {
 				if (c == 0)//adiciona data
 						System.out.println("Data: " +  agenda[li][c] + " nome: " +  agenda[li][c+1] + " Prioridade: " + agenda [li][c+2]);	
-			}
+						}
 			
-		}	
-			}else {
-				System.out.println("========= VocÍ possui idade superior, ligue 1599 =========");
+					}	
+				
+					}else {
+						System.out.println("======================= Dados errados? ========================");
+						System.out.println("========== Atualize a p√°gina para iniciar novamente ===========");
+					}
+							}
+			
+		
+				
+					
 			}
-
+				else {
+			System.out.println("========= Voc√™ possui idade superior, ligue 1599 =========");
+			}
 		}
 	}
-}
+}	 
+	
+
