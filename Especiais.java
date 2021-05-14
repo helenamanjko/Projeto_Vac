d6a97496e5956350d5703b6ca48e4e204bf976f9
Helena Manjko <helenamanjko@gmail.com>
2021-05-14 10:08:25 -0300

    Update Especiais.java
    apresentacao

--- Especiais.java
+++ Especiais.java
@@ -16,24 +16,28 @@ public class Especiais extends Pessoa implements Condicao_Vac {
 	{
 		int resultado;
 		
-		if(i == 1) {
+		if(i == 1) {		//Sindrome de Down
+			resultado = 5;
+		}
+		else if (i == 2)	//Problemas renais em diálise
+		{
 			resultado = 4;
 		}
-		else if (i == 2)
+		else if (i == 3)	//Transplantados Imunossuprimidos
 		{
 			resultado = 3;
 		}
-		else if (i == 3)
+		else if (i == 4)	//Grávidas e Puéperas com comorbidades
 		{
 			resultado = 2;
 		}
-		else if (i == 4)
+		else if (i == 5)	//BPC
 		{
 			resultado = 1;
 		}
 		else
 		{
-			resultado = 0;
+			resultado = 0;	//Nenhuma das Opções
 		}
 		
 		return resultado;	
